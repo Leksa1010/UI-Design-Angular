b155c46b7aa227b7bb8f111ca9c41a7d46740ffa
Leksa1010 <127348571+Leksa1010@users.noreply.github.com>
2024-10-15 00:26:21 +0200

    One step before API
    About to add OMDB API.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {MatChipsModule} from '@angular/material/chips';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -11,7 +12,8 @@ import {MatChipsModule} from '@angular/material/chips';
     MatCardModule,
     MatButtonModule,
     MatChipsModule,
-    MatProgressBarModule
+    MatProgressBarModule,
+    HttpClientModule,
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './home.component.html',
@@ -19,4 +21,14 @@ import {MatChipsModule} from '@angular/material/chips';
 })
 export class HomeComponent {
 
+  public recommended: any[] | undefined = undefined;
+
+  constructor(client: HttpClient) {
+    const url = ''
+    client.get(url, {
+      headers: {
+        'Accept': 'application/json',
+      }
+    }).subscribe(rsp => console.log(rsp))
+  }
 }
