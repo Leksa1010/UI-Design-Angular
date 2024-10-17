039110dfc60e1a58eff659286ba7ace44b6396f6
Leksa1010 <127348571+Leksa1010@users.noreply.github.com>
2024-10-17 19:42:34 +0200

    Added search component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import {Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {AboutComponent} from './about/about.component';
 import {GenreComponent} from './genre/genre.component';
+import {SearchComponent} from './search/search.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'genre', component: GenreComponent},
   {path: 'about', component: AboutComponent},
+  {path: 'search', component: SearchComponent},
   {path: '**', redirectTo: ''}
 ];
