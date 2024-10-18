270d43d03553687f9a3fccd206f1b93351b99be8
Leksa1010 <127348571+Leksa1010@users.noreply.github.com>
2024-10-18 16:52:41 +0200

    Added login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import {MatButton} from "@angular/material/button";
+import {MatCard, MatCardActions, MatCardContent, MatCardHeader} from "@angular/material/card";
+import {MatFormField, MatFormFieldModule, MatLabel} from "@angular/material/form-field";
+import {MatInput, MatInputModule} from "@angular/material/input";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+    imports: [
+        MatButton,
+        MatCard,
+        MatCardActions,
+        MatCardContent,
+        MatCardHeader,
+        MatFormFieldModule,
+        MatInputModule,
+        MatLabel,
+        RouterLink
+    ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
