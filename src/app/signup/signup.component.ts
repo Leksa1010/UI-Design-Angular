f89eadf15e0c481e8edc1f72e8ff32d55ae33888
Leksa1010 <127348571+Leksa1010@users.noreply.github.com>
2024-10-18 11:48:08 +0200

    Worked on Signup functionality

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    RouterLink
+  ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
